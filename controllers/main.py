89f541484e25eac98697506f669b25ab4623890f
Rex Diamante <rexdiamante28@gmail.com>
2017-09-07 10:05:48 +0800

    Removed extra pages

--- controllers/main.py
+++ controllers/main.py
@@ -13,44 +13,44 @@ _logger = logging.getLogger(__name__)
 
 
 class dpo_assistant(http.Controller):
-    @http.route('/services', type='http', auth="public", website=True)
-    def principal(self):
+    #@http.route('/services', type='http', auth="public", website=True)
+    #def principal(self):
         #env = request.env(context=dict(request.env.context, show_address=True, no_tag_br=True))
         # Render page
-        return request.render("dpo_assistant.homepage", {})
+    #    return request.render("dpo_assistant.thankyou", {})
 
-    @http.route('/aboutus', type='http', auth="public", website=True)
-    def aboutus(self):
+    #@http.route('/aboutus', type='http', auth="public", website=True)
+    #def aboutus(self):
         #env = request.env(context=dict(request.env.context, show_address=True, no_tag_br=True))
         # Render page
-        return request.render("dpo_assistant.homepage", {})
+    #    return request.render("dpo_assistant.thankyou", {})
 
 
     #Ignore Mo Muna sir Unless I override mo yung Homepage
     #Overriding Homepage
-    @http.route('/page/homepage', type='http', auth="public", website=True)
-    def homepage(self):
+    #@http.route('/page/homepage', type='http', auth="public", website=True)
+    #def homepage(self):
         #env = request.env(context=dict(request.env.context, show_address=True, no_tag_br=True))
         # Render page
-        return request.render("dpo_assistant.homepage", {})
+    #    return request.render("dpo_assistant.thankyou", {})
 
 
     @http.route('/page/contactus', type='http', auth="public", website=True)
     def contactus(self):
         #env = request.env(context=dict(request.env.context, show_address=True, no_tag_br=True))
         # Render page
-        return request.render("dpo_assistant.homepage", {})
+        return request.render("dpo_assistant.contactus", {})
 
 
 
-    @http.route('/page/subscribe', type='http', auth="public", website=True)
-    def submitdata(self, **kw):
+    #@http.route('/page/subscribe', type='http', auth="public", website=True)
+    #def submitdata(self, **kw):
         #Super ID which is ADMIN 1
-        if not request.uid:
-            request.uid = 1        
+    #    if not request.uid:
+    #        request.uid = 1        
 
         #How to Call a Model in a Controller
-        model_dpo_assistant_subscribers = http.request.env['dpo_assistant.subscribers'].sudo()
+     #   model_dpo_assistant_subscribers = http.request.env['dpo_assistant.subscribers'].sudo()
 
 
         #_logger.info('Name: ' + request.params['partner_name'])
@@ -60,18 +60,18 @@ class dpo_assistant(http.Controller):
         #_logger.info('Message: ' + request.params['partner_message'])
 
         #Creation of New Record
-        result = model_dpo_assistant_subscribers.create({
-            'name' : request.params['name'],
-            'email': request.params['email'],
-            'company': request.params['company'],
-            'contact_number': request.params['name'],
-            'message': request.params['message']
-        })
+    #    result = model_dpo_assistant_subscribers.create({
+    #        'name' : request.params['name'],
+    #        'email': request.params['email'],
+    #        'company': request.params['company'],
+    #        'contact_number': request.params['name'],
+    #        'message': request.params['message']
+    #    })
 
 
 
 
-        return request.render("dpo_assistant.thankyou", {})        
+     #   return request.render("dpo_assistant.thankyou", {})        
 
 
     #Ignore Mo Muna sir Unless I override mo yung Homepage
